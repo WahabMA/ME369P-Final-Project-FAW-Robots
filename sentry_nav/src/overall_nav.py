22386c5edca4ff291a1f84e94f97cb9ef1598c43
TheFieryWolf <yahya@esmartsol.com>
2022-05-04 19:59:45 -0500

    will check for wrong input

--- sentry_nav/src/overall_nav.py
+++ sentry_nav/src/overall_nav.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-from email import message
-from turtle import position
+
 import rospy
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
 from geometry_msgs.msg import Point, Twist
 from math import atan2
 import random
+import sys
 
 
 spypose = Odometry()
@@ -35,14 +35,14 @@ def setSpyTwist(pub,goal):
     inc_x = 0
     inc_y = 0
     vel = Twist()
-    while not(0 < inc_x < 0.1) or not(0 < inc_y < 0.1):
+    while not(0 < abs(inc_x) < 0.1) or not(0 < abs(inc_y) < 0.1):
         x,y,theta = getMsgInfo(spypose)
         # print(x,y)
         inc_x = goal.x - x
         inc_y = goal.y - y
         angle_to_goal = atan2(inc_y,inc_x)
         
-        if 0 < inc_x < 0.1 and 0 < inc_y < 0.1:
+        if 0 < abs(inc_x) < 0.1 and 0 < abs(inc_y) < 0.1:
             vel.linear.x = 0.0
             vel.angular.z = 0.0
         elif angle_to_goal - theta > 0.1:
@@ -61,14 +61,14 @@ def setPlayerTwist(pub,goal):
     inc_x = 0
     inc_y = 0
     vel = Twist()
-    while not(0 < inc_x < 0.1) or not(0 < inc_y < 0.1):
+    while not(0 < abs(inc_x) < 0.1) or not(0 < abs(inc_y) < 0.1):
         x,y,theta = getMsgInfo(playerpose)
         # print(x,y)
         inc_x = goal.x - x
         inc_y = goal.y - y
         angle_to_goal = atan2(inc_y,inc_x)
         
-        if 0 < inc_x < 0.1 and 0 < inc_y < 0.1:
+        if 0 < abs(inc_x) < 0.1 and 0 < abs(inc_y) < 0.1:
             vel.linear.x = 0.0
             vel.angular.z = 0.0
         elif angle_to_goal - theta > 0.1:
@@ -89,38 +89,46 @@ spysub = rospy.Subscriber("/spy/odom",Odometry,getSpyMsg)
 spypub = rospy.Publisher('/spy/cmd_vel',Twist,queue_size=1)
 playersub = rospy.Subscriber('/player/odom',Odometry,getPlayerMsg)
 playerpub = rospy.Publisher('/player/cmd_vel',Twist,queue_size=1)
+# obstaclessub = rospy.Subscriber('/obstacles',obstacles)
 
 spyvel = Twist()
 playervel = Twist()
 
 r = rospy.Rate(50)
 
-spypossiblepos = [1.5,2.5,3.5,4.5]
+spypossiblepos = [0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5]
 
 while True:
     
     playerx,playery,playertheta = getMsgInfo(playerpose)
     playerx = round(playerx*2)/2
     playery = round(playery*2)/2
-    player_input = input("Enter your next move --> ")
-    if player_input == 'w':
-        playergoal = Point(playerx,playery+1,0)
-    elif player_input == 's':
-        playergoal = Point(playerx,playery-1,0)
-    elif player_input == 'a':
-        playergoal = Point(playerx-1,playery,0)
-    elif player_input == 'd':
-        playergoal = Point(playerx+1,playery,0)
-    else:
-        print('wrong input')
-    print(playergoal)
-    print(playerx,playery)
+    
+    validInput = False
+    while not validInput:
+        player_input = input("Enter your next move --> ")
+        if player_input in ['w','W']:
+            playergoal = Point(playerx+1,playery,0)
+            validInput = True
+        elif player_input in ['s','S']:
+            playergoal = Point(playerx-1,playery,0)
+            validInput = True
+        elif player_input in ['d','D']:
+            playergoal = Point(playerx,playery-1,0)
+            validInput = True
+        elif player_input in ['a','A']:
+            playergoal = Point(playerx,playery+1,0)
+            validInput = True
+        else:
+            print('invalid move')
+    # print(playergoal)
+    # print(playerx,playery)
     setPlayerTwist(playerpub,playergoal)
     
     spyx,spyy,spytheta = getMsgInfo(spypose)
     spygoal = Point(8.5,random.choice(spypossiblepos),0)
-    print(spygoal)
-    print(spyx,spyy)
+    # print(spygoal)
+    # print(spyx,spyy)
     setSpyTwist(spypub,spygoal)
     
     
